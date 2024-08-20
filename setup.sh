03b517bb6ec3c689d59be20d397f22b8a6425805
Mac <maclong9@icloud.com>
2024-08-21 00:32:43 +0100

    chore: removed unnecassary comments
    Signed-off-by: Mac <maclong9@icloud.com>

--- setup.sh
+++ setup.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-# Clone the dotfiles repository
 git clone https://github.com/maclong9/dots .config
 
-# Create symlinks for dotfiles
 for file in .config/.*(.); do
   if [[ $file:t != ".git" && $file:t != ".gitignore" && $file:t != "." && $file:t != ".." ]]; then
     ln -s "$file" "$HOME/${file:t}"
   fi
 done
 
-# Install mise
 curl https://mise.run | sh
 eval "$(~/.local/bin/mise activate zsh)"
 mise install
 
-# Source .zshrc
 source $HOME/.zshrc
