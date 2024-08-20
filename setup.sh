142f34be97e96e32c78bc5388b048f09aff3315d
Mac <maclong9@icloud.com>
2024-08-20 09:47:47 +0100

    feat: removed plug for editor usage

--- setup.sh
+++ setup.sh
@@ -9,9 +9,5 @@ done
 curl https://mise.run | sh
 eval "$(~/.local/bin/mise activate zsh)"
 mise install
-$HOME/.local/share/mise/installs/node/lts/bin/npm i -g pnpm vercel
-
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 source $HOME/.zshrc
+npm i -g pnpm vercel
