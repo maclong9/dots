c8cc6d2dabde014f98f3455bc17f45216a164f24
Mac <maclong9@icloud.com>
2024-08-20 09:37:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 git clone https://github.com/maclong9/dots .config
 
 for file in .config/*(D); do
-  if [[ $file:t != ".git" && $file:t != ".gitignore" && $file:t != "setup.sh" ]]; then
+  if [[ $file:t =~ ^\..* && $file:t != ".git" && $file:t != ".gitignore" ]]; then
     ln -s "$file" "$HOME/${file:t}"
   fi
 done
 
-source ~/.zshrc
-
+curl https://mise.run | sh
+eval "$(~/.local/bin/mise activate zsh)"
 mise install
-npm i -g pnpm vercel
+
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl https://mise.run | sh
 
+npm i -g pnpm vercel
 
 source $HOME/.zshrc
