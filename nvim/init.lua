aa4106507d84f945fd5da5f42059587a43c5b001
Mac <maclong9@icloud.com>
2024-08-24 13:50:05 +0100

    feat: added plugins to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,216 +1,244 @@
 -- Enable syntax highlighting
 vim.cmd('syntax enable')
 
--- Set colorscheme
-vim.cmd('colorscheme vesper')
-
--- Set cursor shape
-vim.opt.guicursor = {
-  'n-v-c:block-Cursor/lCursor-blinkon0',
-  'i-ci:ver25-Cursor/lCursor',
-  'r-cr:hor20-Cursor/lCursor'
-}
-
 -- Set global variables
 local globals = {
-  indentLine_char = '│',
-  is_posix = 1,
-  mapleader = ';',
+	is_posix = 1,    -- Use POSIX-compatible behavior
+	mapleader = ';', -- Set the leader key to semicolon
 }
 
+-- Apply global variables
 for k, v in pairs(globals) do
-  vim.g[k] = v
+	vim.g[k] = v
 end
 
--- Set various options
+-- Set Neovim options
 local options = {
-  breakindent = true,
-  cursorline = true,
-  hlsearch = true,
-  incsearch = true,
-  showmode = false,
-  swapfile = false,
-  number = true,
-  relativenumber = true,
-  regexpengine = 0,
-  scrolloff = 999,
-  shiftwidth = 2,
-  signcolumn = 'yes',
-  smartcase = true,
-  smartindent = true,
-  tabstop = 2,
+	breakindent = true,    -- Preserve indentation on wrapped lines
+	cursorline = true,     -- Highlight the current line
+	hlsearch = true,       -- Highlight search results
+	incsearch = true,      -- Show search matches as you type
+	showmode = false,      -- Don't show the mode in the last line
+	swapfile = false,      -- Disable swap file creation
+	number = true,         -- Show line numbers
+	relativenumber = true, -- Show relative line numbers
+	regexpengine = 0,      -- Use automatic regexp engine selection
+	scrolloff = 999,       -- Keep cursor centered vertically
+	shiftwidth = 2,        -- Number of spaces for each indent level
+	signcolumn = 'yes',    -- Always show the sign column
+	smartcase = true,      -- Case-sensitive search if query has uppercase
+	smartindent = true,    -- Smart autoindenting on new lines
+	tabstop = 2,           -- Number of spaces a tab counts for
 }
 
+-- Apply Neovim options
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
--- Key mappings
+-- Set up key mappings
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 local mappings = {
-  {'n', '<C-h>', '<C-w>h'},
-  {'n', '<C-j>', '<C-w>j'},
-  {'n', '<C-k>', '<C-w>k'},
-  {'n', '<C-l>', '<C-w>l'},
-  {'n', '<Esc>', '<cmd>nohlsearch<CR>'},
-  {'n', '<leader>e', '<cmd>Oil<CR>'},
-  {'n', '<leader>b', '<cmd>Telescope buffers<CR>'},
-  {'n', '<leader>f', '<cmd>Telescope find_files<CR>'},
-  {'n', '<leader>g', '<cmd>Telescope live_grep<CR>'},
-  {'n', '<leader>s', '<cmd>Telescope lsp_document_symbols<CR>'},
+	{ 'n', '<C-h>',             '<C-w>h' },                                  -- Move to left window
+	{ 'n', '<C-j>',             '<C-w>j' },                                  -- Move to bottom window
+	{ 'n', '<C-k>',             '<C-w>k' },                                  -- Move to top window
+	{ 'n', '<C-l>',             '<C-w>l' },                                  -- Move to right window
+	{ 'n', '<Esc>',             '<cmd>nohlsearch<CR>' },                     -- Clear search highlight
+	{ 'n', '<leader>b',         '<cmd>Telescope buffers<CR>' },              -- Find Buffers
+	{ 'n', '<leader>f',         '<cmd>Telescope find_files<CR>' },           -- Find Files
+	{ 'n', '<leader>g',         '<cmd>Telescope live_grep<CR>' },            -- Find Text
+	{ 'n', '<leader>s',         '<cmd>Telescope lsp_document_symbols<CR>' }, -- Find Symbols
+
+	-- LSP Mappings
+	{ 'n', '<leader>lh',        '<cmd>lua vim.lsp.buf.hover()<CR>' },        -- Show hover information
+	{ 'n', '<leader>la',        '<cmd>lua vim.lsp.buf.code_action()<CR>' },  -- Show code actions
+	{ 'n', '<leader>ld',        '<cmd>lua vim.lsp.buf.definition()<CR>' },   -- Go to definition
+	{ 'n', '<leader>li',        '<cmd>lua vim.lsp.buf.implementation()<CR>' }, -- Go to implementation
+	{ 'n', '<leader>lt',        '<cmd>lua vim.lsp.buf.type_definition()<CR>' }, -- Go to type definition
+	{ 'n', '<leader>lr',        '<cmd>lua vim.lsp.buf.references()<CR>' },   -- Find references
 }
 
+-- Apply key mappings
 for _, mapping in ipairs(mappings) do
-  keymap(mapping[1], mapping[2], mapping[3], opts)
+	keymap(mapping[1], mapping[2], mapping[3], opts)
 end
 
--- Bootstrap Lazy
+-- Setup LSP Signs with Nerd Fonts
+local signs = { Error = "", Warn = "", Hint = "", Info = " " }
+
+-- Apply signs
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Bootstrap Lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugin specification
 require("lazy").setup({
-  { "windwp/nvim-autopairs", event = "InsertEnter" },
-  { "github/copilot.vim" },
-  { "datsfilipe/vesper.nvim" },
-  { "nvim-telescope/telescope.nvim", tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { "mattn/emmet-vim" },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
-  { "neovim/nvim-lspconfig" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/cmp-buffer" },
-  { "hrsh7th/cmp-path" },
-  { "L3MON4D3/LuaSnip" },
-  { "saadparwaiz1/cmp_luasnip" },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "tpope/vim-commentary" },
-  { "tpope/vim-fugitive" },
-  { "tpope/vim-rsi" },
-  { "tpope/vim-surround" },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-  { "lewis6991/gitsigns.nvim" },
-  { "wellle/targets.vim" },
-  {
-    'stevearc/oil.nvim',
-    opts = {},
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-  }
+	{ "windwp/nvim-autopairs" }, -- Autoclose brackets
+	{ "williamboman/mason.nvim" },          -- LSP package manager
+	{ "williamboman/mason-lspconfig.nvim" }, -- mason.nvim bridge
+	{ "neovim/nvim-lspconfig" },            -- LSP configuration
+	{ "hrsh7th/nvim-cmp" },                 -- Autocompletion plugin
+	{ "hrsh7th/cmp-nvim-lsp" },             -- LSP source for cmp
+	{ "hrsh7th/cmp-buffer" },               -- Buffer source for cmp
+	{ "hrsh7th/cmp-path" },                 -- Path source for cmp
+	{ "L3MON4D3/LuaSnip" },                 -- Snippet engine
+	{ "saadparwaiz1/cmp_luasnip" },         -- Luasnip source for cmp
+	{ "tpope/vim-commentary" },    -- Commenting support
+	{ "tpope/vim-fugitive" },      -- Git integration
+	{ "tpope/vim-rsi" },           -- Readline-style keys
+	{ "tpope/vim-surround" },      -- Surround text objects
+	{ "chrisgrieser/nvim-spider" }, -- Move through camelCase
+	{ "wellle/targets.vim" },     -- Additional text objects
+	{ "folke/neodev.nvim", opts = {} }, -- Neovim Lua development
+  { -- Git signs
+    "lewis6991/gitsigns.nvim", 
+    opts = {} 
+  }, 
+	{                           -- Fuzzy finder
+		"nvim-telescope/telescope.nvim",
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+	},
+	{                                       -- Treesitter integration
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+    opts = {
+      ensure_installed = "all", -- or a list of languages
+    	highlight = {
+    		enable = true,         -- false will disable the whole extension
+    		additional_vim_regex_highlighting = false,
+    	},
+    	indent = {
+    		enable = true
+    	},
+    },
+	},
+	{                              -- Indentation guides
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+    opts = {
+      indent = { char = "│" },
+    }, 
+	},
+	{                             -- File explorer
+		'stevearc/oil.nvim',
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{ -- Formatting
+		'stevearc/conform.nvim',
+		opts = {
+			formatters_by_ft = {
+				markdown = { "deno" },
+				html = { "deno" },
+				typescript = { "deno" },
+				yaml = { "deno" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	}
 })
 
 -- LSP setup
 require('mason').setup()
-require('mason-lspconfig').setup()
+require('mason-lspconfig').setup({
+	ensure_installed = {
+		'cssls',
+		'denols',
+		'eslint',
+		'html',
+		'lua_ls',
+		'marksman',
+		'tailwindcss',
+		'tsserver'
+	},
+	automatic_installation = true,
+})
 
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- Add the LSP servers you want to use
-local servers = { 'deno', 'tsserver', 'rust_analyzer' }
-
--- LSP keymaps
-local on_attach = function(client, bufnr)
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+-- Set up each LSP server
+for _, lsp in ipairs({
+	'cssls',
+	'denols',
+	'eslint',
+	'html',
+	'lua_ls',
+	'marksman',
+	'tailwindcss',
+	'tsserver'
+}) do
+	lspconfig[lsp].setup {
+		on_attach = function(client, bufnr)
+			-- Key mappings for LSP-related commands can be added here
+		end,
+		capabilities = capabilities,
+	}
 end
 
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
-
+-- Set up nvim-cmp for autocompletion
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'buffer' },
-    { name = 'path' },
-  },
-}
-
--- Setup gitsigns
-require('gitsigns').setup()
-
--- Setup indent-blankline
-require('ibl').setup()
-
--- Setup Telescope
-require('telescope').setup{}
-
--- Setup Treesitter
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all", -- or a list of languages
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	}),
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+		{ name = 'buffer' },
+		{ name = 'path' },
+	},
 }
