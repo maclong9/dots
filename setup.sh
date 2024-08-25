de44ebd4f9af1de67ed521f40651b7e9cccd0c8d
Mac <maclong9@icloud.com>
2024-08-25 20:38:53 +0100

    feat(zshrc): added curl call and read of anthropic key

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# curl -sSL https://raw.githubusercontent.com/maclong9//main/setup.sh | sh
+
 git clone https://github.com/maclong9/dots .config
 
 for file in .config/.*(.); do
@@ -11,5 +13,9 @@ done
 curl https://mise.run | sh
 eval "$(~/.local/bin/mise activate zsh)"
 mise install
+
+echo "Please enter your ANTHROPIC_API_KEY:"
+read api_key
+echo "export ANTHROPIC_API_KEY=$api_key" >> ~/.zshenv
 
 source $HOME/.zshrc
