1345e891556dd0dec8fabc730dcb9f74871fbf67
Mac <maclong9@icloud.com>
2024-08-27 10:49:15 +0100

    chore(setup): removed gh configuration and added print of reminder to set it up

--- setup.sh
+++ setup.sh
@@ -14,5 +14,5 @@ mise install
 
 source $HOME/.zshrc
 
-gh auth login
-gh extension install github/gh-copilot
+printf "Configuration Complete\n"
+printf "Make sure to run '\033[0;34mgh auth login && gh extension install github/gh-copilot\033[0m' to configure the GitHub CLI."
