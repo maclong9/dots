e3fd578e7c0293852401d49349a1549b329ae993
Mac <maclong9@icloud.com>
2024-09-23 12:18:20 +0100

    fix(setup): removed unused zplug cron

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ eval "$("$HOME"/.local/bin/mise activate zsh)"
 mise install -y
 
 (crontab -l 2>/dev/null; \
-  echo "0 12 * * 1 /Users/maclong/.local/bin/mise upgrade && . /Users/maclong/.zplug/init.zsh && zplug update" \
+  echo "0 12 * * 1 /Users/maclong/.local/bin/mise upgrade" \
 ) | crontab -
 
 . "$HOME/.zshrc"
