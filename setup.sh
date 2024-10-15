d556f5493341e6d27fa7f511b40a7dc65c45b66c
Mac <maclong9@icloud.com>
2024-10-15 10:14:20 +0100

    remove: caffeination

--- setup.sh
+++ setup.sh
@@ -5,39 +5,36 @@
 trap 'cleanup' EXIT
 cleanup() {
   if [ $? -ne 0 ]; then
-    sudo rm -rf "$HOME"/.config "$HOME"/.gitconfig "$HOME"/.gitignore \
-      "$HOME"/.vim "$HOME"/.vimrc "$HOME"/.zshrc /etc/pam.d/sudo_local
-    (crontab -l 2>/dev/null | sed '$d;$d') | crontab -
+	sudo rm -rf "$HOME"/.config "$HOME"/.gitconfig "$HOME"/.gitignore \
+	  "$HOME"/.vim "$HOME"/.vimrc "$HOME"/.zshrc /etc/pam.d/sudo_local
+	(crontab -l 2>/dev/null | sed '$d;$d') | crontab -
   fi
 }
 
 # check if running on macOS
 if  [ "$(uname -s)" = "Darwin" ]; then
-  # keep machine awake while script is running
-	caffeinate -s -w $$
-
-	# enable Touch ID for `sudo`
-	sudo sed 's/^#auth/auth/' /etc/pam.d/sudo_local.template |
-	  sudo tee /etc/pam.d/sudo_local > /dev/null
+# enable Touch ID for `sudo`
+sudo sed 's/^#auth/auth/' /etc/pam.d/sudo_local.template |
+  sudo tee /etc/pam.d/sudo_local > /dev/null
 
 	# install developer tools
-  if ! xcode-select -p >/dev/null 2>&1; then
-    xcode-select --install
-  fi
+	if ! xcode-select -p >/dev/null 2>&1; then
+		xcode-select --install
+	fi
   
-  # accept developer tools license
-  if ! /usr/bin/xcrun clang >/dev/null 2>&1; then
-    sudo xcodebuild -license accept
-  fi
+	# accept developer tools license
+	if ! /usr/bin/xcrun clang >/dev/null 2>&1; then
+		sudo xcodebuild -license accept
+	fi
 fi
 
 # clone configuration files and symlink to home directory
 git clone https://github.com/maclong9/dots .config
 for file in .config/.*; do
-  case "$(basename "$file")" in
-    "." | ".." | ".git") continue ;;
-    *) ln -s "$file" "$HOME/$(basename "$file")" ;;
-  esac
+	case "$(basename "$file")" in
+		"." | ".." | ".git") continue ;;
+		*) ln -s "$file" "$HOME/$(basename "$file")" ;;
+	esac
 done
 
 # install mise & runtimes then create cron for updating runtimes every Monday at 10
