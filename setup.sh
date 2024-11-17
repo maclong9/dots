4f3670b1043953c0ed4499ad3fab298aeebec4b3
Mac Long <maclong9@icloud.com>
2024-11-17 00:26:16 +0000

    fix(setup): add nvm source

--- setup.sh
+++ setup.sh
@@ -39,6 +39,7 @@ done
 
 # install tooling
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+source "$HOME/.nvim/nvm.sh"
 nvm install 22
 npm i -g tailwindcss-language-server typescript-language-server vscode-langservers-extracted
 
