064050916a35ca42f05b540bf1c4c283e4632089
Mac Long <maclong9@icloud.com>
2025-05-13 16:39:00 +0100

    fix: add deno install

--- setup.sh
+++ setup.sh
@@ -37,4 +37,6 @@ fi
 sudo curl -L "$download_url" -o /usr/local/bin/sls
 sudo chmod +x /usr/local/bin/sls
 
+curl -fsSL https://deno.land/install.sh | sh
+
 printf "Run 'source ~/.zshrc' to and add your SSH key where needed"
